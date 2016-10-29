5ef1ad5fb3b0ff80d88cd9c65fb65c576b17aab8
Patel347 <thepatel347@gmail.com>
2016-10-29 18:07:48 +0100

    added some more fields for the database

--- html/challenges.php
+++ html/challenges.php
@@ -11,17 +11,33 @@
     $servername = 'localhost';
     $username = 'root';
     $password = '';
-    $dbname = 'sql_db';
-    $conn = new mysqli($servername, $username, $password);
+    $dbname = 'spent_db';
+    $conn = new mysqli($servername, $username, $password,$dbname);
 
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     } 
     echo "Connected successfully";
+    echo "<br>";
+    $sql = "SELECT id, Name, Value, Start_Time,End_Time, Description FROM challanges";
+    $result = $conn->query($sql);
 
+    if ($result->num_rows > 0)
+    {
+      // output data of each row
+      while($row = $result->fetch_assoc())
+      {
+        echo "id: " . $row["id"]. " - Name: " . $row["Name"]. "- Points Awarded" . $row["Value"]. $row["Start_Time"].$row["End_Time"]. $row["Description"]."<br>";
+      }
+    }
 
+    else 
+    {
+      echo "0 results";
+    }
 
+    $conn->close();
 
 
     ?>
